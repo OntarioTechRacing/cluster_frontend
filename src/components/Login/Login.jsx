2448879be80abb4e064c3bf999e665818ab7ac2e
Manal Afzal <manal.afzal@ontariotechu.net>
2025-03-02 13:26:19 -0500

    Added login page functionality

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,15 @@
+const Login = ()=>{
+    return(
+        <div>
+            <h1>Create account</h1>
+            <form>
+                <input type="email" placeholder="Enter your email"></input>
+                <input type="text" placeholder="Enter your password"></input>
+            </form>
+
+            <button>Create account</button>
+        </div>
+    )
+}
+
+export default Login
