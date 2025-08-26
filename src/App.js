2d10604698d863b7dd62c90c9301b3a31459917a
RayanK05 <rayan.khalil@ontariotechu.net>
2025-08-26 19:00:21 -0400

    route path for login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SimulationRuns from "./Pages/SimulationRuns.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
 import SimulationComparison from "./Pages/SimulationComparison.jsx";
+import LoginPage from "./Pages/LoginPage.jsx";
 
 function App() {
   return (
